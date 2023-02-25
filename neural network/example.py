4f42f6cabf4d488861d2e5345484817f3ad61833
xiaoxiangyeyu-zxy <74137364+xiaoxiangyeyu-zxy@users.noreply.github.com>
2023-02-25 17:54:41 +0800

    Add files via upload

--- neural network/example.py
+++ neural network/example.py
@@ -7,7 +7,7 @@ v1 = np.ravel(x1)  # 2d->1d
 v2 = np.ravel(x2)  # 2d->1d
 Y = v1**2 + v2**2  # get x1**2+x2**2
 X = np.stack((v1, v2), axis=1)  # reshape to 40000*2
-nn = MLPRegressor(hidden_layer_sizes=(100, 50), activation='relu', solver='adam', alpha=0.0001,)
+nn = MLPRegressor(hidden_layer_sizes=(500, 100), activation='relu', solver='adam', alpha=0.0001,)
 nn.fit(X, Y)
 yptrain = nn.predict(X)
 
@@ -30,8 +30,9 @@ Zp1 = Y.reshape(200, 200)
 plt.contourf(Xp1, Yp1, Zp1, np.linspace(0., 2., 11))
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('true')
-plt.savefig('true.pdf')
+plt.title('true_ex')
+plt.colorbar()
+plt.savefig('true_ex.pdf')
 
 plt.figure()
 Xp2, Yp2 = np.mgrid[-1:1:200j, -1:1:200j]
@@ -39,5 +40,6 @@ Zp2 = yptrain.reshape(200, 200)
 plt.contourf(Xp2, Yp2, Zp2, np.linspace(0., 2., 11))
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('train')
-plt.savefig('train.pdf')
+plt.title('train_ex')
+plt.colorbar()
+plt.savefig('train_ex.pdf')
--- neural network/network_for_table.py
+++ neural network/network_for_table.py
@@ -3,50 +3,83 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x1, x2 = np.mgrid[-10:4:141j, 16:26:101j]  # 141*101
-v1 = np.ravel(x1)  # 2d->1d
-v2 = np.ravel(x2)  # 2d->1d
-v1 = 10**v1
-v2 = 10**v2
+v1 = np.ravel(x1)  # lg(density) 2d->1d
+v2 = np.ravel(x2)  # lg(energy) 2d->1d
+v1m = 10**v1  # density
+v2m = 10**v2  # energy
 X = np.stack((v1, v2), axis=1)  # reshape to (14241)*2
-print(v1)
-print(v2)
+# print(v1)
+# print(v2)
 data = np.loadtxt("table.txt")
-T = data[:, 0]
-P = data[:, 1]
-tt = MLPRegressor(hidden_layer_sizes=(100, 50, 25), activation='relu', solver='adam', alpha=0.0001,)
-pp = MLPRegressor(hidden_layer_sizes=(100, 50, 25), activation='relu', solver='adam', alpha=0.0001,)
+Tm = data[:, 0]  # temperature
+Pm = data[:, 1]  # intensity of pressure
+T = np.log10(Tm)  # lg(tem)
+P = np.log10(Pm)  # lg(press)
+tt = MLPRegressor(hidden_layer_sizes=(200, 50, 25), activation='relu', solver='adam', alpha=0.0001,)
+pp = MLPRegressor(hidden_layer_sizes=(200, 50, 25), activation='relu', solver='adam', alpha=0.0001,)
 tt.fit(X, T)
 pp.fit(X, P)
 ttrain = tt.predict(X)
-pptrain = pp.predict(X)
-print(T)
-print(ttrain)
-print(P)
-print(pptrain)
+ptrain = pp.predict(X)
+ttrainm = 10**ttrain
+ptrainm = 10**ptrain
 
-# # calculate R**2
-# Et = np.mean(Y)
-# print(Et)
-# tminusp = yptrain - Y
-# tminusp2 = [x*y for x, y in zip(tminusp, tminusp)]
-# tminusEt2 = [(x-Et)**2 for x in Y]
-# R2 = 1 - np.sum([x/y for x, y in zip(tminusp2, tminusEt2)])
-# print(R2)
+print("lg(temp):", T)
+print("lg(temp_train):", ttrain)
+print("temp:", Tm)
+print("temp_train", ttrainm)
 
-# plt.figure()
-# Xp1, Yp1 = np.mgrid[-1:1:200j, -1:1:200j]
-# Zp1 = Y.reshape(200, 200)
-# plt.contourf(Xp1, Yp1, Zp1, np.linspace(0., 2., 11))
-# plt.xlabel('x')
-# plt.ylabel('y')
-# plt.title('true')
-# plt.savefig('true.pdf')
-#
-# plt.figure()
-# Xp2, Yp2 = np.mgrid[-1:1:200j, -1:1:200j]
-# Zp2 = yptrain.reshape(200, 200)
-# plt.contourf(Xp2, Yp2, Zp2, np.linspace(0., 2., 11))
-# plt.xlabel('x')
-# plt.ylabel('y')
-# plt.title('train')
-# plt.savefig('train.pdf')
+R2T = 1 - np.sum(np.array(T-ttrain)**2)/np.sum(np.array(T-np.mean(T))**2)
+R2Tm = 1 - np.sum(np.array(Tm-10**ttrain)**2)/np.sum(np.array(Tm-np.mean(Tm))**2)
+print("R2T=", R2T)
+print("R2Tm=", R2Tm)
+
+plt.figure()
+Xp1, Yp1 = 10**(np.mgrid[-10:4:141j, 16:26:101j])
+Zp1 = Tm.reshape(141, 101)
+plt.contourf(Xp1, Yp1, Zp1, np.linspace(0.e10, 9.0e10, 10))
+plt.xlabel('density')
+plt.ylabel('energy')
+plt.title('true_T')
+plt.colorbar()
+plt.savefig('true_T.pdf')
+
+plt.figure()
+Xp2, Yp2 = 10**(np.mgrid[-10:4:141j, 16:26:101j])
+Zp2 = ttrainm.reshape(141, 101)
+plt.contourf(Xp2, Yp2, Zp2, np.linspace(0.e10, 9.0e10, 10))
+plt.xlabel('density')
+plt.ylabel('energy')
+plt.title('train_T')
+plt.colorbar()
+plt.savefig('train_T.pdf')
+
+print("lg(press):", P)
+print("lg(press_train):", ptrain)
+print("press:", Pm)
+print("press_train:", ptrainm)
+
+R2P = 1 - np.sum(np.array(P-ptrain)**2)/np.sum(np.array(P-np.mean(P))**2)
+R2Pm = 1 - np.sum(np.array(Pm-10**ptrain)**2)/np.sum(np.array(Pm-np.mean(Pm))**2)
+print("R2P=", R2P)
+print("R2Pm=", R2Pm)
+
+plt.figure()
+Xp3, Yp3 = 10**(np.mgrid[-10:4:141j, 16:26:101j])
+Zp3 = Pm.reshape(141, 101)
+plt.contourf(Xp3, Yp3, Zp3, np.linspace(0.e29, 3.5e29, 8))
+plt.xlabel('density')
+plt.ylabel('energy')
+plt.title('true_P')
+plt.colorbar()
+plt.savefig('true_P.pdf')
+
+plt.figure()
+Xp4, Yp4 = 10**(np.mgrid[-10:4:141j, 16:26:101j])
+Zp4 = ptrainm.reshape(141, 101)
+plt.contourf(Xp4, Yp4, Zp4, np.linspace(0.e29, 3.5e29, 8))
+plt.xlabel('density')
+plt.ylabel('energy')
+plt.title('train_P')
+plt.colorbar()
+plt.savefig('train_P.pdf')
