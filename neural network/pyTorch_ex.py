5b84ab13000300b74269701689f050ce8a7b2d7e
xiaoxiangyeyu-zxy <74137364+xiaoxiangyeyu-zxy@users.noreply.github.com>
2023-03-02 22:02:26 +0800

    Add files via upload

--- neural network/pyTorch_ex.py
+++ neural network/pyTorch_ex.py
@@ -23,13 +23,13 @@ class Net(nn.Module):
 
 net = Net(1, 20, 1)
 optimizer = torch.optim.Adam(net.parameters(), lr=0.1)  # lr=learning rate
-loss_func = torch.nn.MSELoss()
+loss_func = torch.nn.MSELoss()  # loss function = mse
 
 x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)
 y = x.pow(3)+0.1*torch.randn(x.size())
 x, y = (Variable(x), Variable(y))
 
-
+loss_now = 10086
 for i in range(5000):
     prediction = net(x)  # predict result
     loss = loss_func(prediction, y)  # calculate loss rate
@@ -38,4 +38,6 @@ for i in range(5000):
     loss.backward()  # loss back propagation
     optimizer.step()  # grad optimize
 
-    print(float(loss), i+1)
+    if round(float(loss_now), 4) > round(float(loss), 4):
+        print(float(loss), i+1)
+        loss_now = loss
