1fd6c04431cebd199475e50ff600a36f37abe712
xiaoxiangyeyu-zxy <74137364+xiaoxiangyeyu-zxy@users.noreply.github.com>
2023-03-08 16:38:27 +0800

    dct compression for table

---
+++ dct/dct.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+# import plot_fig
+
+x_shape = 141
+y_shape = 101
+
+x_compression = 70
+y_compression = 50
+
+# 整张图 DCT 变换
+def whole_img_dct(img_f32, x, y):
+    img_dct = cv2.dct(img_f32)            # 进行离散余弦变换
+    # img_dct_log = np.log(abs(img_dct))    # 进行log处理
+    # img_recor = cv2.idct(img_dct)          # 进行离散余弦反变换
+    recor_temp = img_dct[0:x_compression, 0:y_compression]
+    recor_temp2 = np.zeros(img_f32.shape)
+    recor_temp2[0:x_compression, 0:y_compression] = recor_temp
+    img_recor1 = cv2.idct(recor_temp2)
+    return img_recor1
+
+
+data = np.loadtxt("table.txt")
+Tm = data[:, 0]  # temperature
+Pm = data[:, 1]  # intensity of pressure
+T = np.log10(Tm)  # lg(tem)
+P = np.log10(Pm)  # lg(press)
+
+T = T.reshape(141, 101)
+P = P.reshape(141, 101)
+Tm = Tm.reshape(141, 101)
+Pm = Pm.reshape(141, 101)
+
+T_dct = whole_img_dct(T[:x_shape-1, :y_shape-1], x_compression, y_compression)
+# print(T)
+# print(T_dct)
+
+Tm_dct = whole_img_dct(Tm[:x_shape-1, :y_shape-1], x_compression, y_compression)
+# print(Tm)
+# print(Tm_dct)
+
+print(np.around(Tm[:x_shape-1, :y_shape-1]-10**T_dct, 16))
+print(np.around(Tm[:x_shape-1, :y_shape-1]-Tm_dct, 16))
